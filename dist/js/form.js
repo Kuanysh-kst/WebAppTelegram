1a40356a1c1e882c1189d26fa1c8cc65f6c91452
Kuanysh <kuanysh4646@gmail.com>
2023-05-09 21:59:56 +0600

    ref
    Signed-off-by: Kuanysh <kuanysh4646@gmail.com>

--- dist/js/form.js
+++ dist/js/form.js
@@ -32,46 +32,7 @@ form.addEventListener("submit", (e) => {
   tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
   tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
   tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
+
   document.getElementById("user_id") = `${tg.initDataUnsafe.user.id}`;
-
-  btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
-    if (tg.MainButton.isVisible){ //если кнопка показана 
-      tg.MainButton.hide() //скрываем кнопку 
-    }
-    else{ //иначе
-      tg.MainButton.show() //показываем 
-    }
-  })
-
-  let btnED = document.getElementById("btnED"); //получаем кнопку активировать/деактивировать
-  btnED.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
-	if (tg.MainButton.isActive){ //если кнопка показана 
-		tg.MainButton.setParams({"color": "#E0FFFF"}); //меняем цвет
-		tg.MainButton.disable() //скрываем кнопку 
-	}
-	else{ //иначе
-		tg.MainButton.setParams({"color": "#143F6B"}); //меняем цвет
-		tg.MainButton.enable() //показываем 
-	}
-});
-
-Telegram.WebApp.onEvent('mainButtonClicked', function(){
-	tg.sendData("some string that we need to send"); 
-	//при клике на основную кнопку отправляем данные в строковом виде
-});
-
-let usercard = document.getElementById("usercard"); //получаем блок usercard 
-
-let profName = document.createElement('p'); //создаем параграф
-profName.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}
-${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
-//выдем имя, "фамилию", через тире username и код языка
-usercard.appendChild(profName); //добавляем 
-
-let userid = document.createElement('p'); //создаем еще параграф 
-userid.innerText = `${tg.initDataUnsafe.user.id}`; //показываем user_id
-usercard.appendChild(userid); //добавляем
-document.getElementById("user_id") = `${tg.initDataUnsafe.user.id}`;
 
 });
