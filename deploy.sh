4328ee0a870661ff08447944b48f23b22ba0c703
Kuanysh <90975457+Kuanysh-kst@users.noreply.github.com>
2023-06-05 22:47:54 +0600

    little changes
    Signed-off-by: Kuanysh <90975457+Kuanysh-kst@users.noreply.github.com>

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:kuanysh-kst/WebAppTelegram.git master:gh-pages
+git push -f git@github.com:Kuanysh-kst/WebAppTelegram.git master:gh-pages
 
 cd -
