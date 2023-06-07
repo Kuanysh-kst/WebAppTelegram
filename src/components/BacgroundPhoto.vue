059b6687da7582fcd96e1bb76b5efb324f2fc08d
Kuanysh <90975457+Kuanysh-kst@users.noreply.github.com>
2023-06-07 19:58:03 +0600

    added new style for buttons
    Signed-off-by: Kuanysh <90975457+Kuanysh-kst@users.noreply.github.com>

---
+++ src/components/BacgroundPhoto.vue
@@ -0,0 +1,7 @@
+<template>
+<h1>Helloworld</h1>
+</template>
+
+<script>
+
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,19 +5,20 @@
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut ipsum vitae placeat quidem molestiae omnis nulla
             alias, aliquam cupiditate nemo obcaecati ducimus dolorem reprehenderit facere doloribus harum? Modi, voluptas
             molestias.</p>
-
         <router-link to="/todos">Todos</router-link>
+        <BackgroundPhoto></BackgroundPhoto>
+        <hr>
         <div class="menu-icons">
             <div class="row">
                 <router-link to="/todos" class="icon-container">
                     <div class="icon-button">
-                        <img class = "ico" src="../assets/free-icon-font-rocket-lunch-6955751.svg" alt="">
+                        <img class="ico" src="../assets/free-icon-font-rocket-lunch-6955751.svg" alt="">
                     </div>
                     <h3>Search</h3>
                 </router-link>
                 <router-link to="/todos" class="icon-container">
                     <div class="icon-button">
-                        <img class = "ico" src="../assets/free-icon-font-user-3917688.svg" alt="">
+                        <img class="ico" src="../assets/free-icon-font-user-3917688.svg" alt="">
                     </div>
                     <h3>Profile</h3>
                 </router-link>
@@ -25,13 +26,13 @@
             <div class="row">
                 <router-link to="/todos" class="icon-container">
                     <div class="icon-button">
-                        <img class = "ico" src="../assets/free-icon-font-cross-3917759.svg" alt="">
+                        <img class="ico" src="../assets/free-icon-font-cross-3917759.svg" alt="">
                     </div>
                     <h3>Close app</h3>
                 </router-link>
                 <router-link to="/todos" class="icon-container">
                     <div class="icon-button">
-                        <img class = "ico" src="../assets/free-icon-font-settings-3917058.svg" alt="">
+                        <img class="ico" src="../assets/free-icon-font-settings-3917058.svg" alt="">
                     </div>
                     <h3>Settings</h3>
                 </router-link>
@@ -40,7 +41,27 @@
     </div>
 </template>
 
+<script>
+import BackgroundPhoto from '@/components/BacgroundPhoto.vue'
+export default {
+    components: {
+        BackgroundPhoto
+    }
+}
+</script>
+
 <style>
+hr {
+    border: none;
+    /* Убираем стандартную границу */
+    background-color: grey;
+    /* Задаем желаемый цвет фона горизонтальной линии */
+    height: 2px;
+    /* Задаем желаемую толщину горизонтальной линии */
+    border-radius: 1px;
+    width: 90%;
+}
+
 .menu-icons {
     height: 400px;
 }
@@ -59,7 +80,7 @@
     height: 100%;
     background: white;
     border-radius: 25px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
     /* Параметры тени */
     justify-content: space-between;
     align-items: center;
@@ -73,7 +94,7 @@
     margin-right: 20px;
     margin-left: 20px;
 }
-    
+
 .ico {
     width: 50%;
     height: 50%;
@@ -81,6 +102,9 @@
     margin-right: 10%;
     margin-left: 10%;
     margin-top: 30px;
-
 }
-</style>
+
+.icon-container h3 {
+    color: black;
+    font-size: 16px;
+}</style>
