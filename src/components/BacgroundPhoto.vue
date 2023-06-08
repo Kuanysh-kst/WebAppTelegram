e4c13938a3881a3898bb5c783b75e768eeb5c359
Kuanysh <90975457+Kuanysh-kst@users.noreply.github.com>
2023-06-08 14:42:42 +0600

    added new avatar
    Signed-off-by: Kuanysh <90975457+Kuanysh-kst@users.noreply.github.com>

--- src/components/BacgroundPhoto.vue
+++ src/components/BacgroundPhoto.vue
@@ -3,24 +3,52 @@
         <div class="photo-container">
             <img class="back-photo" src="../assets/backgroundPhoto.svg" alt="">
         </div>
+        <img class="top-photo" src="../assets/8137014_1575.jpg" alt="">
     </div>
 </template>
 
 <style>
+.back-photo {
+    position: absolute;
+    align-items: center;
+}
+
 .photo-container {
     position: relative;
     overflow: hidden;
-    height: 140px;
+    height: 110px;
     margin-bottom: 70px;
 }
-.back-photo{
-    border-radius: 20px;
 
+.back-photo {
+    border-radius: 20px;
     position: absolute;
     top: 50%;
     width: 90%;
     height: 100%;
-    transform: translate(-50%,-50%);
+    transform: translate(-50%, -50%);
     object-fit: cover;
+}
+
+.top-container {
+    overflow: hidden;
+    margin-bottom: 70px;
+    position: absolute;
+}
+
+.top-photo {
+    position: absolute;
+    top: -150px;
+    border-radius: 50%;
+    top: 100%;
+    width: 100px;
+    height: 100px;
+    transform: translate(-50%, -50%);
+    object-fit: cover;
+    border: 4px solid white;
+}
+
+.photo-row {
+    position: relative;
 }
 </style>
