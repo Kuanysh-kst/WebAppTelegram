aff7d46f8ff263957fa38c8d18172502835a1299
Kuanysh <90975457+Kuanysh-kst@users.noreply.github.com>
2023-06-10 23:09:01 +0600

    added new box
    Signed-off-by: Kuanysh <90975457+Kuanysh-kst@users.noreply.github.com>

--- src/views/ParametrsPage.vue
+++ src/views/ParametrsPage.vue
@@ -3,18 +3,21 @@
         <BackLine />
         <FullNameParam />
         <ExperieceParam />
+        <AddPhotoParam />
     </div>
 </template>
 
 <script>
 import BackLine from '@/components/search/BackLine.vue';
 import FullNameParam from '@/components/param/FullNameParam.vue';
-import ExperieceParam from '@/components/param/ExperienceParam.vue'
+import ExperieceParam from '@/components/param/ExperienceParam.vue';
+import AddPhotoParam from '@/components/param/AddPhotoParam.vue';
 export default {
     components: {
     BackLine,
     FullNameParam,
-    ExperieceParam
+    ExperieceParam,
+    AddPhotoParam
 }
 }
 </script>
